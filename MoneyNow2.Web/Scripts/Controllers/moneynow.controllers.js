439c4751c8b0b9227003066f77512ea582ca7bd3
Alan R. Soares <alanrsoars@gmail.com>
2013-01-15 19:58:42 -0200

    Almost-stable version ;)

---
+++ MoneyNow2.Web/Scripts/Controllers/moneynow.controllers.js
@@ -0,0 +1,33 @@
+﻿var GoogleFinanceConverterAPI = (function () {
+    function GoogleFinanceConverterAPI() {
+        this.serviceUrl = 'http://www.google.com/finance/converter';
+    };
+
+    GoogleFinanceConverterAPI.prototype.getConversion = function (amount, from, to, callback) {
+        $.get(this.serviceUrl, {'a':amount, 'from':from, 'to':to}, function (data) {
+            var result = $(data.responseText).find('span.bld').text();
+            callback(result);
+        });
+    };
+
+    return GoogleFinanceConverterAPI;
+
+})();
+
+var MoneyNowController = function ($scope, $http) {
+    $http.get('/api/currencyinfos').success(function (data) {
+        $scope.currencies = data;
+    });
+
+    $scope.convert = function () {
+        new GoogleFinanceConverterAPI()
+            .getConversion($scope.amount, $scope.from, $scope.to, function (result) {
+
+                var splitted = result.split(" ");
+                
+                $("#result")
+                    .show()
+                    .html("Result: <strong>" + splitted[0] + "</strong>");
+            });
+    };
+};
